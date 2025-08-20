e7caa0514c8895ab487cac6a825a2fb229f19201
jorgeernestotayruiz <tayocruz1772@gmailcom>
2025-08-20 14:29:51 -0600

    Creación de clases base

--- Code.py
+++ Code.py
@@ -3,10 +3,37 @@ class Actividades:
         self.actividades  = {}
 
 class Actividad:
-    def __init__(self,ID,Nombre,Fecha):
+    def __init__(self,ID,nombre,fecha,hora,prioridad,curso):
         self.ID = ID
-        self.Nombre = Nombre
-        self.Fecha = Fecha
-        self.Categoria = ""
-        self.prioridad = ""
+        self.nombre = nombre
+        self.fecha = fecha
+        self.hora = hora
+        self.categoria = ""
+        self.prioridad = prioridad
+        self.curso = curso
+
+class Clase(Actividad):
+    def __init__(self,ID,nombre,fecha,hora,prioridad,curso):
+        super().__init__(ID,nombre,fecha,hora,prioridad,curso)
+        self.categoria = "Clase"
+
+class Examen(Actividad):
+    def __init__(self,ID,nombre,fecha,hora,prioridad,curso):
+        super().__init__(ID,nombre,fecha,hora,prioridad,curso)
+        self.categoria = "Examen"
+
+class Tarea(Actividad):
+    def __init__(self,ID,nombre,fecha,hora,prioridad,curso):
+        super().__init__(ID,nombre,fecha,hora,prioridad,curso)
+        self.categoria = "Tarea"
+
+class Reunion(Actividad):
+    def __init__(self,ID,nombre,fecha,hora,prioridad,curso):
+        super().__init__(ID,nombre,fecha,hora,prioridad,curso)
+        self.categoria = "Reunion"
+
+class Evento(Actividad):
+    def __init__(self,ID,nombre,fecha,hora,prioridad,curso):
+        super().__init__(ID,nombre,fecha,hora,prioridad,curso)
+        self.categoria = "Evento"
 
